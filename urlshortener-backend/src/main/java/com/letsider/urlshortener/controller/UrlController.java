61c7622f83b32a134ac090bc636ab939eae01908
letsider <letsider@gmail.com>
2021-08-16 12:01:25 -0400

    Déplacement des traitements pré service dans le controller

--- urlshortener-backend/src/main/java/com/letsider/urlshortener/controller/UrlController.java
+++ urlshortener-backend/src/main/java/com/letsider/urlshortener/controller/UrlController.java
@@ -4,7 +4,10 @@ import com.letsider.urlshortener.models.Url;
 import com.letsider.urlshortener.repositories.UrlRepository;
 import com.letsider.urlshortener.services.IUrlService;
 
+import com.letsider.urlshortener.utils.Utils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +27,15 @@ public class UrlController {
     @CrossOrigin()
     @GetMapping("shortenUrl")
     public ResponseEntity<Url> shortenUrl(String originalUrl) {
+        Url errorUrl = new Url();
+        if (originalUrl == null || originalUrl.length() == 0) {
+            errorUrl.setHashedUrl("Veuillez entrer un URL");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorUrl);
+        } else if (!Utils.isUrlValid(originalUrl)) {
+            errorUrl.setCompleteUrl(originalUrl);
+            errorUrl.setHashedUrl("URL non valide");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorUrl);
+        }
 
         ResponseEntity<Url> response = urlService.shortenUrl(originalUrl);
 
@@ -34,9 +46,14 @@ public class UrlController {
     @CrossOrigin()
     @GetMapping("retrieveUrl")
     public ResponseEntity<Url> retrieveUrl(String hashedUrl) {
+        Url errorUrl = new Url();
 
-        ResponseEntity<Url> response = urlService.retrieveUrl(hashedUrl);
 
-        return response;
+        if (hashedUrl == null || hashedUrl.isEmpty()) {
+            errorUrl.setHashedUrl("Veuillez entrer un URL");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorUrl);
+        }
+
+        return urlService.retrieveUrl(hashedUrl);
     }
 }
--- urlshortener-backend/src/main/java/com/letsider/urlshortener/services/impl/UrlService.java
+++ urlshortener-backend/src/main/java/com/letsider/urlshortener/services/impl/UrlService.java
@@ -5,7 +5,6 @@ import com.letsider.urlshortener.services.IUrlService;
 import com.letsider.urlshortener.repositories.UrlRepository;
 
 import java.util.zip.CRC32;
-import com.letsider.urlshortener.utils.Utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,14 +27,6 @@ public class UrlService implements IUrlService {
     @Override
     public ResponseEntity<Url> shortenUrl(String url) {
         Url errorUrl = new Url();
-        if (url == null || url.length() == 0) {
-            errorUrl.setHashedUrl("Veuillez entrer un URL");
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorUrl);
-        } else if (!Utils.isUrlValid(url)) {
-            errorUrl.setCompleteUrl(url);
-            errorUrl.setHashedUrl("URL non valide");
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorUrl);
-        }
 
         CRC32 crc32 = new CRC32();
         crc32.update(url.getBytes());
@@ -58,25 +49,15 @@ public class UrlService implements IUrlService {
 
     @Override
     public ResponseEntity<Url> retrieveUrl(String hashedUrl) {
-        Url url = null;
         Url errorUrl = new Url();
-        try {
-
-            if (hashedUrl == null || hashedUrl.isEmpty()) {
-                errorUrl.setHashedUrl("Veuillez entrer un URL");
-                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorUrl);
-            } else {
-                url = urlRepository.findByHashedUrl(hashedUrl);
-            }
-            if (url == null) {
-                errorUrl.setCompleteUrl("URL raccourcie inexistante");
-                errorUrl.setHashedUrl(hashedUrl);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorUrl);
-            }
-
+        Url url = urlRepository.findByHashedUrl(hashedUrl);
+        if (url == null) {
+            errorUrl.setCompleteUrl("URL raccourcie inexistante");
+            errorUrl.setHashedUrl(hashedUrl);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorUrl);
+        } else {
             return new ResponseEntity<>(url, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
     }
 }
